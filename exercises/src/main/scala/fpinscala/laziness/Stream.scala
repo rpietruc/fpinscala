1c2edf3c3f7ac31cffd1bbf0b6a9f5f929e678ad
Rafal Pietruch <rpietruch@piap.pl>
2015-01-23 09:19:47 +0000

    Gen forAll implemented

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -5,11 +5,11 @@ trait Stream[+A] {
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case Cons(h, t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean = 
+  def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
@@ -17,7 +17,17 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = sys.error("todo")
+
+  def take(n: Int): Stream[A] =
+    uncons match {
+      case Some(c) =>
+        if (n > 0)
+          Stream.cons(c.h(), c.t().take(n - 1))
+        else
+          Stream.empty
+
+      case _ => Stream.empty
+    }
 
   def drop(n: Int): Stream[A] = sys.error("todo")
 
@@ -26,9 +36,35 @@ trait Stream[+A] {
   def forAll(p: A => Boolean): Boolean = sys.error("todo")
 
   def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
+
+  def uncons: Option[Cons[A]]
+
+  def toList: List[A] = uncons match {
+    case None => Nil
+    case Some(c) => c.h() :: c.t().toList
+  }
+
+  def map[B](f: A => B): Stream[B] =
+    foldRight(Stream[B]())((a, s) => Stream.cons(f(a), s))
+
+  def zipWith[B, C](that: Stream[B])(f: (A, B) => C): Stream[C] =
+    unfold((this, that)) {
+      case (ss1, ss2) =>
+        ss1.uncons.flatMap(c1 =>
+          ss2.uncons.map(c2 =>
+            (f(c1.h(), c2.h()), (c1.t(), c2.t()))))
+    }
+
+  def zip[B, C](that: Stream[B]): Stream[(A, B)] = zipWith(that)((a, b) => (a, b))
+
 }
-case object Empty extends Stream[Nothing]
-case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+case object Empty extends Stream[Nothing] {
+  override val uncons = None
+}
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A] {
+  val uncons = Some(this)
+}
 
 object Stream {
   def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -13,11 +13,48 @@ The library developed in this chapter goes through several iterations. This file
 shell, which you can fill in and modify while working through the chapter.
 */
 
-trait Prop {
+case class Prop(run: (MaxSize,TestCases,RNG) => Result) {
+  def &&(p: Prop): Prop = Prop { (max, n, rng) => or(run(max, n, rng), p.run(max, n, rng))}
+
+  def ||(p: Prop): Prop = Prop { (max, n, rng) => and(run(max, n, rng), p.run(max, n, rng))}
 }
 
 object Prop {
-  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+
+  type MaxSize = Int
+  type TestCases = Int
+  type FailedCase = String
+  type SuccessCount = Int
+  type Result = Option[(FailedCase, SuccessCount)]
+
+  def forAll[A](g: SGen[A])(f: A => Boolean): Prop =
+    forAll(g.forSize)(f)
+
+  def forAll[A](g: Int => Gen[A])(f: A => Boolean): Prop = Prop {
+    (max, n, rng) =>
+      val casesPerSize = (n + (max - 1)) / max
+      val props: Stream[Prop] =
+        Stream.from(0).take((n min max) + 1).map(i => Gen.forAll(g(i))(f))
+      val prop: Prop =
+        props.map(p => Prop { (max, _, rng) =>
+          p.run(max, casesPerSize, rng)
+        }).toList.reduceLeft(_ && _)
+      prop.run(max, n, rng)
+  }
+
+  def or[A](o1: Option[A], o2: Option[A]): Option[A] = o1 match {
+    case None => o2
+    case Some(a) => Some(a)
+  }
+
+  def and[A](o1: Option[A], o2: Option[A]): Option[A] = o1 match {
+    case None => None
+    case Some(a) => o2
+  }
+
+  /* Produce an infinite random stream from a `Gen` and a starting `RNG`. */
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
+    Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
 }
 
 object Gen {
@@ -29,15 +66,29 @@ object Gen {
     int.flatMap(i => if (i == Integer.MIN_VALUE) positiveInt else State.unit(i))
 
   def choose(start: Int, stopExclusive: Int): Gen[Int] =
-    Gen(positiveInt.map(i => start + i%(stopExclusive - start)))
+    Gen(positiveInt.map(i => start + i % (stopExclusive - start)))
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (max, n, rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+      case (a, i) => try {
+        if (f(a)) None else Some((a.toString, i))
+      } catch {
+        case e: Exception => Some((buildMsg(a, e), i))
+      }
+    }.find(_.isDefined).getOrElse(None)
+  }
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+      s"generated an exception: ${e.getMessage}\n" +
+      s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+
 }
+
 case class Gen[A](sample: State[RNG,A]) {
-  def map[A,B](f: A => B): Gen[B] = ???
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+  def map[A, B](f: A => B): Gen[B] = ???
+
+  def flatMap[A, B](f: A => Gen[B]): Gen[B] = ???
 }
 
-
-trait SGen[+A] {
-
-}
-
+case class SGen[A](forSize: Int => Gen[A])
